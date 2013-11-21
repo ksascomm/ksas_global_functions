cc2d15acf01d5ebe6196274826f49f49ecfe035c
Cara Peckens <cpeckens@gmail.com>
2013-11-21 08:35:10 -0500

    Add mime-types for LaTex

--- ksas-global-functions.php
+++ ksas-global-functions.php
@@ -42,6 +42,7 @@ License: GPL2
 		9.6 Walker class to add page IDs as classes
 	10.0 Global Shortcodes
 		10.1 Custom Menu
+	11.0 Add Mime Types for LaTex files
 
 /*****************1.0 SECURITY AND PERFORMANCE FUNCTIONS*****************************/
 	// 1.1 Prevent login errors - attacker prevention
@@ -820,4 +821,24 @@ function submenu_get_children_ids( $id, $items ) {
 	}
 	
 	add_shortcode( "custommenu", "ksas_custom_menu_shortcode" );
+/*******************11.0 MIME TYPES FOR LATEX UPLOADS******************/
+	add_filter('upload_mimes','add_tex_mime');
+	function add_tex_mime($mimes) {
+	  $mimes['latex'] = 'application/x-latex';
+	  $mimes['tex'] = 'application/x-tex';
+	  $mimes['bib'] = 'application/x-tex';
+	  $mimes['sty'] = 'application/x-tex';
+	  $mimes['dvi'] = 'application/x-dvi';
+	  $mimes['ps'] = 'application/postscript';
+	  return $mimes;
+	}
+	
+	add_filter('wp_mime_type_icon', 'tex_mime_type_icon', 10, 3);
+	function tex_mime_type_icon($icon, $mime, $post_id) {
+		if ( $mime == 'application/x-latex' || $mime == 'application/x-tex' )
+			return wp_mime_type_icon('text');
+		if ( $mime == 'application/x-dvi' || $mime == 'application/postscript' )
+			return wp_mime_type_icon('document');
+		return $icon;
+	}
 ?>
